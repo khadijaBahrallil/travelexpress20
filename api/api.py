b644b243735b43cfdeb43135d9021c9ffc803279
unknown <nicolas.bogalheiro@gmail.com>
2020-11-08 18:49:31 +0100

    update

--- api/api.py
+++ api/api.py
@@ -1,15 +1,16 @@
 import hashlib
 import sqlite3
-from datetime import timedelta
+from datetime import timedelta, datetime
 from functools import wraps
-
-from flask import jsonify, g, request, abort, render_template, redirect, session, url_for, flash
-from flask_api import FlaskAPI, status
+import re
+from flask import g, request, abort, render_template, redirect, session, url_for
+from flask_api import FlaskAPI
 from flask_cors import CORS
 import phonenumbers
 from validate_email import validate_email
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField
+from wtforms.fields.html5 import DateTimeLocalField
 from wtforms.validators import DataRequired
 import win32api
 
@@ -28,7 +29,7 @@ CORS(api)
 
 api.config['SECRET_KEY'] = 'ec3f0831f0978bdc130ebba668551da9afb8740938af64999364262d4ce06d71'
 
-api.permanent_session_lifetime = timedelta(minutes=0.5)  # La session dure 2 heures
+api.permanent_session_lifetime = timedelta(minutes=5)  # La session dure 2
 
 
 # JSON user
@@ -46,7 +47,8 @@ def user(user_id, username, email, password, phone, authorized, forbiddens):
 
 # JSON trip
 
-def trip(trip_id, driver, passengers, departure_date, arrival_date, origin, destination, cost, is_full):
+def trip(trip_id, driver, passengers, departure_date, arrival_date, origin, destination, cost, places_count,
+         places_total):
     return {
         'trip_id': trip_id,
         'driver': driver,
@@ -56,9 +58,32 @@ def trip(trip_id, driver, passengers, departure_date, arrival_date, origin, dest
         'origin': origin,
         'destination': destination,
         'cost': cost,
-        'is_full': is_full
+        'places_count': places_count,
+        'places_total': places_total,
 
     }
+
+
+# Fonctions de repérage des espaces
+
+def blank(string):
+    res = len(string)
+
+    for i in range(len(string)):
+
+        if string[i] == ' ':
+            res -= 1
+
+    return res
+
+
+def not_blank(string):
+    boolean = False
+
+    if len(string.split(' ')) == 1:
+        boolean = True
+
+    return boolean
 
 
 # Fonction de hashage pour les passwords
@@ -73,6 +98,13 @@ def encrypt_string(hash_string):
 # Fonction de validation de l'username
 
 def conditions_username(username):
+    # On récupère la liste des usernames
+
+    temp = query_db('select username from user')
+
+    for i in range(len(temp)):
+        temp[i] = temp[i][0]
+
     if blank(username) > 64:
 
         abort(400, "La longueur maximale du nom est 64 caractères")
@@ -84,6 +116,10 @@ def conditions_username(username):
     elif '@' in username:
 
         abort(400, "@ est interdit dans la définition d'un username")
+
+    elif username in temp:
+
+        abort(400, "L'username existe déjà")
 
     else:
         pass
@@ -153,6 +189,79 @@ def conditions_password(password):
     else:
 
         pass
+
+
+# Fonction de vérification de nombres dans une string
+
+def hasNumbers(inputString):
+    return bool(re.search(r'\d', inputString))
+
+
+# Fonction de validation d'une ville
+
+def condition_city(city):
+    if hasNumbers(city):
+        abort(400, 'Une ville ne doit pas contenir de nombres')
+    else:
+        pass
+
+
+# Fonction de validation du cardNumber
+
+def condition_cardNumber(cardNumber):
+    regex = re.compile("([0-9][0-9][0-9][0-9])\ ([0-9][0-9][0-9][0-9])\ ([0-9][0-9][0-9][0-9])\ ([0-9][0-9][0-9][0-9])")
+    search = regex.search(cardNumber)
+    if search is not None:
+
+        if search.group(0) != cardNumber:
+            abort(400, 'Le numéro saisi est invalide')
+
+    else:
+        abort(400, 'Le numéro saisi est invalide')
+
+
+# Fonction de validation de la date d'expiration
+
+def condition_expireDate(expireDate):
+    regex = re.compile("([0-9][0-9])\/([0-9][0-9])")
+    search = regex.search(expireDate)
+    if search is not None:
+
+        if search.group(0) == expireDate and 1 <= int(expireDate[0:2]) <= 12 and (int(expireDate[3:5]) > int(
+                str(datetime.now().year)[2:4]) or (int(expireDate[3:5])== int(
+                str(datetime.now().year)[2:4]) and int(expireDate[0:2])>= int(datetime.now().month))):
+            pass
+        else:
+            abort(400, "La date d'expiration est invalide")
+
+    else:
+        abort(400, "La date d'expiration est invalide")
+
+
+# Fonction de validation du cryptogramme
+
+def condition_cryptogram(cryptogram):
+    regex = re.compile("([0-9]+)")
+    search = regex.search(cryptogram)
+    if search is not None:
+
+        if len(cryptogram) != 3 and len(cryptogram) != 4:
+            abort(400, 'Le cryptogramme est invalide')
+    else:
+        abort(400, 'Le cryptogramme est invalide')
+
+# Fonction de validation du titulaire de la carte
+
+def condition_owner(owner):
+    regex=re.compile("([A-Z]+)\ ([A-Z]+)")
+    search = regex.search(owner)
+    if search is not None:
+        if search.group(0)!=owner:
+            abort(400,'Le nom du titulaire est invalide')
+        else:
+            pass
+    else:
+        abort(400,'Le nom du titulaire est invalide')
 
 
 # Décorateur d'authentification
@@ -263,28 +372,43 @@ def trip_create():
     if form.validate_on_submit():
 
         result = request.form
-        # driver = result['driver']
+        driver = session['username']
+
+        origin = result['origin']
+        condition_city(origin)
+        destination = result['destination']
+        condition_city(destination)
         departure_date = result['departure_date']
         arrival_date = result['arrival_date']
-        origin = result['origin']
-        destination = result['destination']
-        cost = result['cost']
-        is_full = 'False'
+        now = datetime.today().strftime('%Y-%m-%dT%H:%M')
+
+        if departure_date > arrival_date:
+            abort(400, "La date de départ doit être antérieure à la date d'arrivée")
+        elif departure_date < now or arrival_date < now:
+            abort(400, "Les dates doivent correspondre ou être ultérieures à la date d'aujourd'hui ")
+        else:
+            pass
+
+        cost = 5
+        places_total = result['places_total']
 
         try:
 
             # Création du trip
 
             insert_trip = query_db(
-                'insert into trip(departure_date,arrival_date,origin,destination,cost,is_full) VALUES(?,?,?,?,?,?)',
-                [departure_date, arrival_date, origin, destination, int(cost), is_full], change=True)
+                'insert into trip(driver,departure_date,arrival_date,origin,destination,cost,places_count,places_total,passengers) VALUES(?,?,?,?,?,?,?,?,?)',
+                [driver, departure_date, arrival_date, origin, destination, cost, int(places_total),
+                 int(places_total), ''],
+                change=True)
 
         except():
 
             abort(500)
 
         else:
-            win32api.MessageBox(0, 'Trip well created', 'TravelExpress', 0x00001000)
+            win32api.MessageBox(0, 'Création du trip effectuée avec succès', 'TravelExpress', 0x00001000)
+            return redirect(url_for('my_trips'))
 
     return render_template("trip_create.html", title='Create a trip', form=form)
 
@@ -292,9 +416,9 @@ def trip_create():
 class TripForm(FlaskForm):
     origin = StringField('Origin', validators=[DataRequired()])
     destination = StringField('Destination', validators=[DataRequired()])
-    cost = IntegerField('Cost', validators=[DataRequired()])
-    departure_date = DateField('Departure Date', format='%Y-%m-%d', validators=[DataRequired()])
-    arrival_date = DateField('Arrival Date', format='%Y-%m-%d', validators=[DataRequired()])
+    departure_date = DateTimeLocalField('Departure Date', validators=[DataRequired()], format='%Y-%m-%dT%H:%M')
+    arrival_date = DateTimeLocalField('Arrival Date', validators=[DataRequired()], format='%Y-%m-%dT%H:%M')
+    places_total = IntegerField('Total Places', validators=[DataRequired()])
     submit = SubmitField('Add Trip')
 
 
@@ -309,21 +433,28 @@ def trip_search():
 
         result = request.form
         origin = result['origin']
+        condition_city(origin)
         destination = result['destination']
+        condition_city(destination)
         departure_date = result['departure_date']
+        places_count = int(result['places_count'])
+
+        if places_count <= 0:
+            abort(400, 'Le nombre de places est invalide')
 
         try:
             trips = api_trip()
             search = []
             for t in trips:
-                if (t['origin'] == origin) and (t['destination'] == destination) and (
-                        t['departure_date'] == departure_date):
+                if (t['origin']== origin) and (t['destination'] == destination) and (
+                        departure_date in t['departure_date']) and (places_count <= t['places_count']):
                     search.append(t)
+
         except():
             abort(500)
 
         else:
-            return render_template("trip_search_result.html", title='Trip Search Result', trips=trips)
+            return render_template("trip_search_result.html", title='Trip Search Result', trips=search)
 
     return render_template("trip_search.html", title='Search Trip', form=form)
 
@@ -331,7 +462,8 @@ def trip_search():
 class TripSearchForm(FlaskForm):
     origin = StringField('Origin', validators=[DataRequired()])
     destination = StringField('Destination', validators=[DataRequired()])
-    departure_date = DateField('Departure Date', validators=[DataRequired()])
+    departure_date = DateField('Departure Date', validators=[DataRequired()], format='%Y-%m-%d')
+    places_count = IntegerField('Places', validators=[DataRequired()])
     submit = SubmitField('Search Trip')
 
 
@@ -354,11 +486,14 @@ def user_search():
                 if u['username'] == username:
                     search.append(u)
 
+            if len(search) == 1:
+                search = search[0]
+
         except():
             abort(500)
 
         else:
-            return render_template("user_search_result.html", title="User Search Result", users=users)
+            return render_template("user_search_result.html", title="User Search Result", user=search)
 
     return render_template("user_search.html", title='Search User', form=form)
 
@@ -402,7 +537,7 @@ def signup_page():
 
         try:
 
-            # Creation de l'user
+            # Création de l'user
 
             insert_user = query_db(
                 'insert into user(username,email,password,phone,authorized,forbiddens) VALUES(?,?,?,?,?,?)',
@@ -454,11 +589,9 @@ def api_users():
 # Profil de l'user connecté
 
 def profil():
-
     username = session['username']
     infos = query_db('select * from user where username=?', [username], one=True)
     return user(infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6])
-
 
 
 # Profil de l'user
@@ -466,7 +599,6 @@ def profil():
 @api.route('/user_profile.html', methods=['GET', 'PATCH'])
 @logged_in
 def user_profile():
-
     profile = profil()
     form = SignupForm()
 
@@ -522,133 +654,90 @@ def api_trip():
     trips = query_db('select * from trip order by trip_id')
 
     for i in range(len(trips)):
-        trips[i] = trip(trips[i][0], trips[i][1], trips[i][2], trips[i][3], trips[i][4], trips[i][5],
-                        trips[i][6], trips[i][7], trips[i][8])
+        trips[i] = trip(trips[i][0], trips[i][1], trips[i][2].split(None), trips[i][3], trips[i][4], trips[i][5],
+                        trips[i][6], trips[i][7], trips[i][8], trips[i][9])
 
     return trips
 
 
-@api.route('/trip/<int:trip_id>/', methods=['GET', 'PATCH', 'DELETE'])
-def trip_trip_id(trip_id):
+# Trips de l'user en cours
+
+@api.route('/mytrips.html')
+@logged_in
+def my_trips():
+    all_trips = api_trip()
+    driver = []
+    passenger = []
+    for t in all_trips:
+        if t['driver'] == session['username']:
+            driver.append(t)
+        if session['username'] in t['passengers']:
+            passenger.append(t)
+        else:
+            pass
+
+    return render_template("mytrips.html", title="My Trips", trips_driver=driver,trips_passenger=passenger)
+
+
+# Paiement
+
+@api.route("/checkout.html/<int:trip_id>", methods=['GET', 'POST'])
+@logged_in
+def checkout(trip_id):
+    infos = query_db('select * from trip where trip_id=?', [trip_id], one=True)
+
     try:
 
-        infos = query_db('select * from trip where trip_id=?', [trip_id], one=True)
+        if not infos:
 
-        if request.method == "GET":
+            abort(404, 'Trip not found with the given trip_id')
 
-            try:
+        else:
 
-                if not infos:
-
-                    abort(404, 'Trip not found with the given trip_id')
-
-                else:
-
-                    profil = trip(infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6],
-                                  infos[7], infos[8])
-
-                    return jsonify(profil), status.HTTP_200_OK
-
-            except():
-
-                abort(500)
-
-
-        elif request.method == "PATCH":
-
-            try:
-
-                # Driver
-
-                driver = request.data.get('driver', None)
-
-                # Passengers
-
-                passengers = request.data.get('passengers', None)
-
-                # Departure date
-
-                departure_date = request.data.get('departure_date', None)
-
-                # Arrival date
-
-                arrival_date = request.data.get('arrival_date', None)
-
-                # Origin
-
-                origin = request.data.get('origin', None)
-
-                # Destination
-
-                destination = request.data.get('destination', None)
-
-                # Cost
-
-                cost = request.data.get('cost', None)
-
-                # Full
-
-                is_full = request.data.get('is_full', None)
-
-                # Update of the trip
-
-                update_trip = query_db(
-                    'update trip set driver=?,passengers=?,departure_date=?,arrival_date=?,origin=?,destination=?,cost=?,is_full=? where user_id=?',
-                    [driver, passengers, departure_date, arrival_date, origin, destination, cost, is_full,
-                     is_full, trip_id], change=True)
-
-                return trip(trip_id, driver, passengers, departure_date, arrival_date, origin, destination, cost,
-                            is_full), status.HTTP_200_OK
-
-
-            except():
-
-                abort(500)
-
-        else:  # request.method ='DELETE'
-
-            try:
-
-                # Get the list of trip_ids
-
-                liste = query_db('select trip_id from trip')
-
-                temp = []
-
-                for element in liste:
-                    temp += element
-
-                if trip_id in temp:
-
-                    delete_user = query_db('delete from trip where trip_id=?', [trip_id], change=True)
-
-                    return {}, status.HTTP_204_NO_CONTENT
-
-                else:
-
-                    abort(404, 'Trip has already been deleted or does not exist')
-
-
-            except():
-
-                abort(500)
+            book_trip = trip(infos[0], infos[1], infos[2], infos[3], infos[4], infos[5], infos[6],
+                             infos[7], infos[8], infos[9])
 
     except():
 
         abort(500)
 
-
-@api.route("/checkout.html")
-def checkout():
     form = CheckoutForm()
-    return render_template("checkout.html", title='Checkout', form=form)
+
+    if form.validate_on_submit():
+        result = request.form
+        owner = result['username']
+        condition_owner(owner)
+        cardNumber = result['cardNumber']
+        condition_cardNumber(cardNumber)
+        expireDate = result['expireDate']
+        condition_expireDate(expireDate)
+        cryptogram = result['cryptogram']
+        condition_cryptogram(cryptogram)
+        passenger = " "+session['username']
+
+
+        try:
+
+            update_trip = query_db(
+                'update trip set passengers=passengers || ?,places_count=places_count-1 where trip_id=?',
+                [passenger, trip_id], change=True)
+
+        except():
+
+            abort(500)
+
+        else:
+            win32api.MessageBox(0, 'Paiement effectué avec succès', 'TravelExpress', 0x00001000)
+            return redirect(url_for('mytrips'))
+
+    return render_template("checkout.html", title='Check Out', form=form, trip=book_trip)
 
 
 class CheckoutForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    cardNumber = IntegerField('Card Number', validators=[DataRequired()])
+    username = StringField('Card Owner',validators=[DataRequired()])
+    cardNumber = StringField('Card Number', validators=[DataRequired()])
     expireDate = StringField('Expire Date', validators=[DataRequired()])
-    cryptogram = IntegerField('Cryptogram', validators=[DataRequired()])
+    cryptogram = StringField('Cryptogram', validators=[DataRequired()])
     submit = SubmitField('Confirm Transaction')
 
 
@@ -691,27 +780,7 @@ def internal_error_server(e):
     return error(e), 500
 
 
-# Fonctions de repérage des espaces
-
-def blank(string):
-    res = len(string)
-
-    for i in range(len(string)):
-
-        if string[i] == ' ':
-            res -= 1
-
-    return res
-
-
-def not_blank(string):
-    boolean = False
-
-    if len(string.split(' ')) == 1:
-        boolean = True
-
-    return boolean
-
+# Nom de la base de données
 
 DATABASE = 'TravelExpress.db'
 
